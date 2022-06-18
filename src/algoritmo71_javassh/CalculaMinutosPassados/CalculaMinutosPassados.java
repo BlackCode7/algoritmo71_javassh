9013dfe65f397927c7d77376d8347545914cc7c4
Anderson <BlackCode7@outlook.com>
2022-06-17 22:02:52 -0300

    atualizando

--- src/algoritmo71_javassh/CalculaMinutosPassados/CalculaMinutosPassados.java
+++ src/algoritmo71_javassh/CalculaMinutosPassados/CalculaMinutosPassados.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class CalculaMinutosPassados {
 		
 	public static void main(String[] args) {
-		CalculaMinutosPassados();
+		CalculaMinutosPassados();//
 	}
 
 	private static void CalculaMinutosPassados(){
