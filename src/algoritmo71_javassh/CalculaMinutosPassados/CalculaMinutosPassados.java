2fe73b4ca9c460ec21c0bbf2f2c98bc664d0fd0d
Anderson <BlackCode7@outlook.com>
2022-06-17 21:56:03 -0300

    First commit

---
+++ src/algoritmo71_javassh/CalculaMinutosPassados/CalculaMinutosPassados.java
@@ -0,0 +1,23 @@
+package algoritmo71_javassh.CalculaMinutosPassados;
+
+import java.util.Scanner;
+
+public class CalculaMinutosPassados {
+		
+	public static void main(String[] args) {
+		CalculaMinutosPassados();
+	}
+
+	private static void CalculaMinutosPassados(){
+		
+		Scanner scanner = new Scanner(System.in);
+		int hora = scanner.nextInt();
+		int minuto = scanner.nextInt();
+		int tempoPassado = 0;
+		
+		tempoPassado = hora * 60 + minuto;
+		
+		System.out.println("Hora: "+hora+" Minuto: "+minuto+" TempoPassado: "+ tempoPassado);
+		
+	}
+}
